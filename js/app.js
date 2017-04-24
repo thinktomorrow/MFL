49bc902ffa438f66423fc0703c95dba48406ab30
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-04-24 14:36:33 +0200

    MFL life is the best life

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).on('click', '.MFL', function(e){
+    e.preventDefault();
+    play();
+});
+
+ function play(){
+    $('#audio').get(0).play();
+ }
+ function stop(){
+     $('#audio').get(0).pause();
+ }
+ $('.key a').click(function(e){
+    $(this).toggleClass('activate');
+    checkAll();
+ });
+
+ var checkAll = function() {
+ var allDivs = $(".key a");
+ var classedDivs = $(".key a.activate");
+
+ var allDivsHaveClass = (allDivs.length === classedDivs.length);
+
+if (allDivsHaveClass) {
+    $('.sliding-doors').addClass('opened');
+    $('body').addClass('warning');
+    setTimeout(function(){
+        $('#launchtime').css('z-index', '5');
+        $('.sliding-doors').css('background-color', '#191919');
+    }, 1300);
+}else{
+    $('body').removeClass('warning');
+    $('#launchtime').css('z-index', '-5');
+    $('.sliding-doors').removeClass('opened');
+    $('.sliding-doors').css('background-color', 'transparent');
+    stop();
+}
+};
