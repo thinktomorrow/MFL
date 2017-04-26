016813afdaba37d2b8e83ebc52cbfbc755a17f6e
Albert Tomasiak <tomasiakalbert@gmail.com>
2017-04-26 09:04:41 +0200

    MFL version 2.0

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 $(document).on('click', '.MFL', function(e){
     e.preventDefault();
-    play();
+    togglePlay();
 });
+
+var playing = 0;
+function togglePlay(){
+    if(playing == 0){
+        play();
+        playing++;
+        return playing;
+    }else{
+        stop();
+        playing--;
+        return playing;
+    }
+}
 
  function play(){
     $('#audio').get(0).play();
@@ -14,24 +27,39 @@ $(document).on('click', '.MFL', function(e){
     checkAll();
  });
 
- var checkAll = function() {
- var allDivs = $(".key a");
- var classedDivs = $(".key a.activate");
+var checkAll = function() {
+    var allDivs = $(".key a");
+    var classedDivs = $(".key a.activate");
+    var allDivsHaveClass = (allDivs.length === classedDivs.length);
 
- var allDivsHaveClass = (allDivs.length === classedDivs.length);
-
-if (allDivsHaveClass) {
-    $('.sliding-doors').addClass('opened');
-    $('body').addClass('warning');
-    setTimeout(function(){
-        $('#launchtime').css('z-index', '5');
-        $('.sliding-doors').css('background-color', '#191919');
-    }, 1300);
-}else{
-    $('body').removeClass('warning');
-    $('#launchtime').css('z-index', '-5');
-    $('.sliding-doors').removeClass('opened');
-    $('.sliding-doors').css('background-color', 'transparent');
-    stop();
+    if (allDivsHaveClass) {
+        $('.sliding-doors').addClass('opened');
+        $('body').addClass('warning');
+        // Slight delay
+        setTimeout(function(){
+            $('.grabber').addClass('glitch');
+            $('#launchtime').css('z-index', '5');
+            $('#launchtime').css('background-color', '#191919');
+            isTheSafetyReallyOff();
+        }, 1300);
+    }else{
+        $('body').removeClass('warning');
+        $('.sliding-doors').removeClass('opened');
+        $('.grabber').removeClass('glitch');
+        $('#launchtime').css('z-index', '-5');
+        stop();
+        playing = 0;
+        return playing;
+    }
 }
-};
+
+function isTheSafetyReallyOff(){
+    var allDivs = $(".key a");
+    var classedDivs = $(".key a.activate");
+    var allDivsHaveClass = (allDivs.length === classedDivs.length);
+    if(!allDivsHaveClass){
+        $('.sliding-doors::before').css('z-index', '6');
+        $('.sliding-doors::after').css('z-index', '5');
+         $('#launchtime').css('z-index', '-1');
+    }
+}
