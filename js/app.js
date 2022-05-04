2b87ce17312381c8b13f11f23871d8e6e51c7144
Ben Cavens <cavensben@gmail.com>
2022-05-04 16:38:09 +0200

    add confetti

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
+var jsConfetti = new JSConfetti();
+
+var clicked = false;
+
 $(document).on('click', '.MFL', function(e){
+
+    // Do not click more than once dog.
+    if(clicked) return;
+    clicked = true;
+
     e.preventDefault();
     $('#audio').get(0).play();
+
     setTimeout(function(){
        $('#iframe').toggleClass('hidden');
        $('#audio').get(0).pause();
        $('#suspense').get(0).pause();
 
-    },5000);
+        $('#winning').get(0).play();
+        sprinkleConfetti();
+
+    },4000);
     // redirect();
 });
+
+function sprinkleConfetti()
+{
+    var max = 50;
+
+    setInterval(function(){
+
+        if(max === 0) return;
+        max -= 1;
+        console.log(max);
+
+        jsConfetti.addConfetti({
+            confettiNumber: Math.round(Math.random() * 300),
+        });
+    }, 600)
+
+}
 
 
 
  function redirect(){
-    var delay = 5000; 
+    var delay = 5000;
     var url = 'https://kalibermaatwerk.be'
     setTimeout(function(){ window.location = url; }, delay);
  }
